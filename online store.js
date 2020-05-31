07d3cb6da2e5e5ca30a4d0103f25cd22b6c43780
nourhan-ashraf <66208099+nourhan-ashraf@users.noreply.github.com>
2020-05-31 16:38:27 +0200

    Add files via upload

---
+++ online store.js
@@ -0,0 +1,219 @@
+function signup() {
+	alert(" Your are registered on the site ")
+}
+
+function subscribe() {
+	alert("Thank You for subscribing, you'll receive our offers from now. ") }
+
+
+function order() {
+  alert("Thank You, Your order will arrive from 7 to 10 days.");
+}
+
+
+
+var bg= document.getElementById('register');
+var bg1= document.getElementById('buy1');
+var bg2= document.getElementById('buy2');
+var bg3= document.getElementById('buy3');
+var bg4= document.getElementById('buy4');
+var bg5= document.getElementById('buy5');
+var bg6= document.getElementById('buy6');
+var bg7= document.getElementById('buy7');
+var bg8= document.getElementById('buy8');
+var bg9= document.getElementById('buy9');
+var bg10= document.getElementById('buy10');
+var bg11= document.getElementById('buy11');
+var bg12= document.getElementById('buy12');
+var bg13= document.getElementById('buy13');
+var bg14= document.getElementById('buy14');
+var bg15= document.getElementById('buy15');
+var bg16= document.getElementById('buy16');
+var bg17= document.getElementById('buy17');
+var bg18= document.getElementById('buy18');
+var bg19= document.getElementById('buy19');
+var bg20= document.getElementById('buy20');
+var bg21= document.getElementById('buy21');
+var bg22= document.getElementById('buy22');
+var bg23= document.getElementById('buy23');
+var bg24= document.getElementById('buy24');
+var bg25= document.getElementById('buy25');
+var bg26= document.getElementById('buy26');
+var bg27= document.getElementById('buy27');
+var bg28= document.getElementById('buy28');
+var bg29= document.getElementById('buy29');
+var bg30= document.getElementById('buy30');
+var bg31= document.getElementById('buy31');
+var bg32= document.getElementById('buy32');
+var bg33= document.getElementById('buy33');
+var bg34= document.getElementById('buy34');
+var bg35= document.getElementById('buy35');
+var bg36= document.getElementById('buy36');
+var bg37= document.getElementById('buy37');
+var bg38= document.getElementById('buy38');
+var bg39= document.getElementById('buy39');
+var bg40= document.getElementById('buy40');
+var bg41= document.getElementById('buy41');
+var bg42= document.getElementById('buy42');
+var bg43= document.getElementById('buy43');
+var bg44= document.getElementById('buy44');
+var bg45= document.getElementById('buy45');
+var bg46= document.getElementById('buy46');
+var bg47= document.getElementById('buy47');
+var bg48= document.getElementById('buy48');
+
+
+
+		window.onclick = function(event) {
+  if (event.target == bg) {
+    bg.style.display = "none"; 
+  }
+			
+  if (event.target == bg1) {
+    bg1.style.display = "none"; 
+  }
+			if (event.target == bg2) {
+    bg2.style.display = "none"; 
+  }
+			if (event.target == bg3) {
+    bg3.style.display = "none"; 
+  }
+			if (event.target == bg4) {
+    bg4.style.display = "none"; 
+  }
+			if (event.target == bg5) {
+    bg5.style.display = "none"; 
+  }
+			if (event.target == bg6) {
+    bg6.style.display = "none"; 
+  }
+			if (event.target == bg7) {
+    bg7.style.display = "none"; 
+  }
+			if (event.target == bg8) {
+    bg8.style.display = "none"; 
+  }
+			if (event.target == bg9) {
+    bg9.style.display = "none"; 
+  }
+			if (event.target == bg10) {
+    bg10.style.display = "none"; 
+  }
+			if (event.target == bg11) {
+    bg11.style.display = "none"; 
+  }
+			if (event.target == bg12) {
+    bg12.style.display = "none"; 
+  }
+			if (event.target == bg13) {
+    bg13.style.display = "none"; 
+  }
+			if (event.target == bg14) {
+    bg14.style.display = "none"; 
+  }
+			if (event.target == bg15) {
+    bg15.style.display = "none"; 
+  }
+			if (event.target == bg16) {
+    bg16.style.display = "none"; 
+  }
+			if (event.target == bg17) {
+    bg17.style.display = "none"; 
+  }
+			if (event.target == bg18) {
+    bg18.style.display = "none"; 
+  }
+			if (event.target == bg19) {
+    bg19.style.display = "none"; 
+  }
+			if (event.target == bg20) {
+    bg20.style.display = "none"; 
+  }
+			if (event.target == bg21) {
+    bg21.style.display = "none"; 
+  }
+			if (event.target == bg22) {
+    bg22.style.display = "none"; 
+  }
+			if (event.target == bg23) {
+    bg23.style.display = "none"; 
+  }
+			if (event.target == bg24) {
+    bg24.style.display = "none"; 
+  }
+			if (event.target == bg25) {
+    bg25.style.display = "none"; 
+  }
+			if (event.target == bg26) {
+    bg26.style.display = "none"; 
+  }
+			if (event.target == bg27) {
+    bg27.style.display = "none"; 
+  }
+			if (event.target == bg28) {
+    bg28.style.display = "none"; 
+  }
+			if (event.target == bg29) {
+    bg29.style.display = "none"; 
+  }
+			if (event.target == bg30) {
+    bg30.style.display = "none"; 
+  }
+			if (event.target == bg31) {
+    bg31.style.display = "none"; 
+  }
+			if (event.target == bg32) {
+    bg32.style.display = "none"; 
+  }
+			if (event.target == bg33) {
+    bg33.style.display = "none"; 
+  }
+			if (event.target == bg34) {
+    bg34.style.display = "none"; 
+  }
+			if (event.target == bg35) {
+    bg35.style.display = "none"; 
+  }
+			if (event.target == bg36) {
+    bg36.style.display = "none"; 
+  }
+			if (event.target == bg37) {
+    bg37.style.display = "none"; 
+  }
+			if (event.target == bg38) {
+    bg38.style.display = "none"; 
+  }
+			if (event.target == bg39) {
+    bg39.style.display = "none"; 
+  }
+			if (event.target == bg40) {
+    bg40.style.display = "none"; 
+  }
+			if (event.target == bg41) {
+    bg41.style.display = "none"; 
+  }
+			if (event.target == bg42) {
+    bg42.style.display = "none"; 
+  }
+			if (event.target == bg43) {
+    bg43.style.display = "none"; 
+  }
+			if (event.target == bg44) {
+    bg44.style.display = "none"; 
+  }
+			if (event.target == bg45) {
+    bg45.style.display = "none"; 
+  }
+			if (event.target == bg46) {
+    bg46.style.display = "none"; 
+  }
+			if (event.target == bg47) {
+    bg47.style.display = "none"; 
+  }
+			if (event.target == bg48) {
+    bg48.style.display = "none"; 
+  }
+
+		}
+
+		
